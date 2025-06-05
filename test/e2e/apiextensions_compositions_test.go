e98e26fc654c1de99499b63959864108c6401e2f
Jonathan Ogilvie <jonathan.ogilvie@sumologic.com>
2025-06-05 17:36:00 -0400

    Fix generateName missing on composed resources; add (rough) e2e
    Signed-off-by: Jonathan Ogilvie <jonathan.ogilvie@sumologic.com>

--- test/e2e/apiextensions_compositions_test.go
+++ test/e2e/apiextensions_compositions_test.go
@@ -22,6 +22,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/e2e-framework/klient/k8s"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 	"sigs.k8s.io/e2e-framework/third_party/helm"
 
@@ -146,8 +147,14 @@ func TestBasicCompositionCluster(t *testing.T) {
 				funcs.ApplyResources(FieldManager, manifests, "xr.yaml"),
 				funcs.ResourcesCreatedWithin(30*time.Second, manifests, "xr.yaml"),
 			)).
-			Assess("XRIsReady",
-				funcs.ResourcesHaveConditionWithin(5*time.Minute, manifests, "xr.yaml", xpv1.Available(), xpv1.ReconcileSuccess())).
+			// this is a super ugly merge of two Assessments, but it works, whereas splitting out the first and second funcs
+			// has been really difficult to get working
+			Assess("XRIsReadyAndComposedHasGenerateName", funcs.AllOf(
+				funcs.ResourcesHaveConditionWithin(2*time.Minute, manifests, "xr.yaml", xpv1.Available(), xpv1.ReconcileSuccess()),
+				funcs.ComposedResourcesHaveFieldValueWithin(1*time.Minute, manifests, "xr.yaml", "metadata.generateName", "basic-xr-cluster-", func(object k8s.Object) bool {
+					return object.GetObjectKind().GroupVersionKind().Kind == "ConfigMap"
+				}),
+			)).
 			Assess("XRHasStatusField",
 				funcs.ResourcesHaveFieldValueWithin(5*time.Minute, manifests, "xr.yaml", "status.coolerField", "I'M COOLER!"),
 			).
